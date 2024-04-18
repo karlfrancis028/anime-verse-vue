66d0799888cabe5e3d74a288b3683340d73d35fa
karlfrancis028 <kflinget@gmail.com>
2024-04-18 22:07:32 +0800

    Added showNav option

--- src/components/one-col-layout.vue
+++ src/components/one-col-layout.vue
@@ -1,17 +1,20 @@
 <script setup lang="ts">
   interface OneColLayoutProps {
     isLoading: boolean;
+    showNav?: boolean;
   }
 
   withDefaults(defineProps<OneColLayoutProps>(), {
     isLoading: false,
+    showNav: true,
   });
 </script>
 
 <template>
   <blocking-loader v-if="isLoading" />
-  <div v-else class="one-col-layout container">
-    <navbar/>
+  <div v-else class="one-col-layout container"
+              :class="{'has-nav': showNav}">
+    <navbar v-if="showNav"/>
     <slot name="toolbar"></slot>
     <div class="one-col-layout__content">
       <slot></slot>
